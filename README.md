fa3ce361d8b57bac81508db6df4a6ac7a911dbcc
Rishabh gangwar <64923428+rishab-gangwar@users.noreply.github.com>
2021-12-06 03:25:03 +0000

    edited readme.md Added two more problem of W9

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# PDSA(https://onlinedegree.iitm.ac.in/course_pages/BSCCS2002.html)
+# PDSA
+(https://onlinedegree.iitm.ac.in/course_pages/BSCCS2002.html)
 # `programming data structures and algorithms using python`
-This is diploma level course taught by  Madhavan Mukund
+This is diploma level course taught by:
+
+ `Madhavan Mukund`
+ 
 Director, Department of Computer Science & Engineering, Chennai Mathematical Institute 
+
+```
+WEEK 1 	  Python refresher
+WEEK 2 	  O() notation, Arrays, lists stacks, queues, heaps
+WEEK 3 	  Sorting and searching
+WEEK 4 	  Sorting and searching (Continued)
+WEEK 5 	  Graph Algorithms
+WEEK 6 	  Graph Algorithms (Continued)
+WEEK 7 	  Divide and Conquer
+WEEK 8 	  Abstract datatypes
+WEEK 9 	  Abstract datatypes (Continued)
+WEEK 10 	Dynamic programming
+WEEK 11 	Dynamic programming (Continued)
+WEEK 12 	Network flows and linear programming
+```
---
+++ codes/W9/P3.py
@@ -0,0 +1,49 @@
+# 650. 2 Keys Keyboard
+"""
+Medium
+
+There is only one character 'A' on the screen of a notepad. You can perform two operations on this notepad for each step:
+
+    Copy All: You can copy all the characters present on the screen (a partial copy is not allowed).
+    Paste: You can paste the characters which are copied last time.
+
+Given an integer n, return the minimum number of operations to get the character 'A' exactly n times on the screen.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: 3
+Explanation: Intitally, we have one character 'A'.
+In step 1, we use Copy All operation.
+In step 2, we use Paste operation to get 'AA'.
+In step 3, we use Paste operation to get 'AAA'.
+
+Example 2:
+
+Input: n = 1
+Output: 0
+
+ 
+
+Constraints:
+
+    1 <= n <= 1000
+
+"""
+class Solution:
+    def minSteps(self, n: int) -> int:
+        ans=0
+        i=2
+        q=n
+        while(i<=n**(.5)+1):
+            if q%i==0:
+                ans+=i
+                q=q//i                
+            else:
+                i+=1
+        if q!=1:
+            ans+=q
+        return ans
+        
